fbbfd257cf54db2d09d38de195a452a7fa25b567
谭炳健 <tbj621@163.com>
2020-07-27 17:20:15 +0800

    测试解压成功

--- src/main/java/teclan/springboot/dao/UpdateDao.java
+++ src/main/java/teclan/springboot/dao/UpdateDao.java
@@ -5,6 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import teclan.springboot.utils.Assert;
 
+import java.util.List;
 import java.util.Map;
 
 @Repository
@@ -18,11 +19,11 @@ public class UpdateDao {
      * @return
      */
     public String getPatch(String verion) throws Exception {
-        Map<String, Object> map = jdbcTemplate.queryForMap("SELECT PATH FROM VERSION_RECORD WHERE VERSION=? ",verion);
 
-        if(Assert.assertNull(map) || Assert.assertNull(map.get("PATH"))){
-            throw new Exception(String.format("版本号 % 有误，找不到对应的补丁文件",verion));
+      List<Map<String, Object>> list =  jdbcTemplate.queryForList(String.format("SELECT PATH FROM VERSION_RECORD WHERE VERSION='%s' ",verion));
+        if(Assert.assertNull(list) ||list.size()==0 || Assert.assertNull(list.get(0))|| Assert.assertNull(list.get(0).get("PATH"))){
+            throw new Exception(String.format("版本号 %s 有误，找不到对应的补丁文件",verion));
         }
-        return map.get("PATH").toString();
+        return list.get(0).get("PATH").toString();
     }
 }
--- src/main/java/teclan/springboot/services/UpdateService.java
+++ src/main/java/teclan/springboot/services/UpdateService.java
@@ -5,10 +5,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import teclan.springboot.controller.UpdateController;
 import teclan.springboot.dao.UpdateDao;
 import teclan.springboot.utils.FileUtils;
 import teclan.springboot.utils.ResultUtils;
+import teclan.springboot.utils.WinRAR;
 
 @Service
 public class UpdateService {
@@ -19,16 +19,32 @@ public class UpdateService {
     public JSONObject doUpdate(String version){
 
         try{
-           String path = updateDao.getPatch(version);
-
+          String path = updateDao.getPatch(version);
+            //String  path = "E:\\Codes\\openSource\\teclan-auto-update\\teclan-auto-update-0.0.1.zip";
            if(!FileUtils.exists(path)){
                throw new Exception(String.format("版本号:%s，补丁文件:%s 不存在"));
            }
            // 开始升级
+            String finalPath = path;
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        WinRAR.unzip(finalPath);
+                    } catch (Exception e) {
+                        LOGGER.error(e.getMessage(),e);
+                    }
+                }
+            }).start();
             return ResultUtils.getResult(200,"升级成功");
         }catch (Exception e){
             LOGGER.error(e.getMessage(),e);
             return ResultUtils.getResult(200,"升级失败",e.getMessage());
         }
     }
+
+
+
+
 }
